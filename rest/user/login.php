c5d4f2edd6dfadf0ee6a68e643a7df4ab02f0ddd
Justus Chua <justuschua@hotmail.com>
2018-10-14 22:47:31 +0800

    remove error log

--- rest/user/login.php
+++ rest/user/login.php
@@ -30,8 +30,6 @@ if (isset($input['user_username']) && isset($input['user_password'])) {
     
     // Retrieve login credentials using username
     if($user->retrieveLoginCred()){
-        error_log(hash_password($input_password, $user->salt));
-        error_log($user->user_password);
         if((hash_password($input_password, $user->salt) == $user->user_password)){
             $response["message"] = "Login successful";
             $response["user_name"] = $user->user_name;
