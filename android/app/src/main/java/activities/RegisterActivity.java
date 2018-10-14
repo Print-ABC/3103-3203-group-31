b005e9c79c672bd219bbac62d8d7ba8cebb5631c
Justus Chua <justuschua@hotmail.com>
2018-10-15 00:16:43 +0800

    update password regex for stronger password

--- android/app/src/main/java/activities/RegisterActivity.java
+++ android/app/src/main/java/activities/RegisterActivity.java
@@ -167,7 +167,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private boolean validatePassword() {
-        String regx = "(^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,20}$)";
+        String regx = "(?=^.{16,35}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$";
         Pattern pattern = Pattern.compile(regx);
         Matcher matcher = pattern.matcher(password);
 
