aa716cec70ead1356cac625d08931b6366a9c75e
Justus Chua <justuschua@hotmail.com>
2018-10-17 15:04:24 +0800

    removed unused files

--- android/app/src/main/java/services/UserService.java
+++ android/app/src/main/java/services/UserService.java
@@ -2,23 +2,14 @@ package services;
 
 import models.Result;
 import models.User;
-import models.UserList;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 import retrofit2.http.Url;
 
 public interface UserService {
-    @GET("users/.")
-    Call<UserList> getUserList();
-
     @GET
     Call<User> getUser(@Url String url);
 
