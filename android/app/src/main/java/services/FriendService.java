204c731b4bf73c407a5fec492f3dc2142a3548f3
Tan Suan Khai <mail.abcwxyz@gmail.com>
2018-10-22 23:53:09 +0800

    Implement FriendService methods

--- android/app/src/main/java/services/FriendService.java
+++ android/app/src/main/java/services/FriendService.java
@@ -3,14 +3,34 @@ package services;
 import java.util.List;
 import models.FriendRequest;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 public interface FriendService {
     @GET(".")
-    Call<List<FriendRequest>> getAllRelations();
+    Call<List<FriendRequest>> getAllRequests();
 
-    @POST("relationship/relationship")
-    Call<FriendRequest> retrieveFriends(@Query("id") String friend_one_id);
+    @GET("req/{uid}")
+    Call<List<FriendRequest>> getByRequesterID();
+
+    @GET("recp/{uid}")
+    Call<List<FriendRequest>> getByRecipientID();
+
+    @POST(".")
+    Call<FriendRequest> createRequest(@Body FriendRequest body);
+
+    @PATCH("{uid}/add/{fuid}")
+    Call<FriendRequest> addFriend();
+
+    @PATCH("{uid}/del/{fuid}")
+    Call<FriendRequest> deleteFriend();
+
+    @DELETE("{uid}")
+    Call<FriendRequest> deleteRequest();
+
+//    @POST("relationship/relationship")
+//    Call<FriendRequest> retrieveFriends(@Query("id") String friend_one_id);
 }
