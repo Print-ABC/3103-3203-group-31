758c970cc2d6cb25a1fa6c7b5c8e982dd54a17cf
Tan Suan Khai <mail.abcwxyz@gmail.com>
2018-10-26 16:44:03 +0800

    Edit FriendSearchFragment.java and FriendPendingFragment.java

--- android/app/src/main/java/fragments/FriendSearchFragment.java
+++ android/app/src/main/java/fragments/FriendSearchFragment.java
@@ -60,34 +60,38 @@ public class FriendSearchFragment extends Fragment {
                 call.enqueue(new Callback<User>() {
                     @Override
                     public void onResponse(Call<User> call, Response<User> response) {
-                        if(response.code() == 200) {
-                            tvName.setText(response.body().getName());
-                            tvUsername.setText("Username : " + response.body().getUsername());
-                            btnAdd.setVisibility(View.VISIBLE);
-                            btnAddOnClickListener(response.body().getUid(), response.body().getName(), response.body().getUsername());
-                        } else if(response.code() == 404) {
-                            disableForm("User not found");
-                        } else {
-                            disableForm("Search failed");
+                        switch (response.code()) {
+                            case 200:
+                                tvName.setText(response.body().getName());
+                                tvUsername.setText("Username : " + response.body().getUsername());
+                                btnAdd.setVisibility(View.VISIBLE);
+                                btnAddOnClickListener(response.body().getUid(), response.body().getName(), response.body().getUsername());
+                                break;
+                            case 404:
+                                disableForm("User not found");
+                                break;
+                            default:
+                                disableForm("Search failed");
+                                break;
                         }
                     }
+
                     @Override
-                    public void onFailure(Call<User> callU, Throwable t) {
-                    }
+                    public void onFailure(Call<User> callU, Throwable t) {}
                 });
             }
         });
         return view;
     }
 
-    public void disableForm(String msg){
+    public void disableForm(String msg) {
         Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
         tvName.setText(null);
         tvUsername.setText(null);
         btnAdd.setVisibility(View.INVISIBLE);
     }
 
-    public void btnAddOnClickListener(final String f_uid, final String f_name, final String f_username){
+    public void btnAddOnClickListener(final String f_uid, final String f_name, final String f_username) {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -99,16 +103,17 @@ public class FriendSearchFragment extends Fragment {
                 call.enqueue(new Callback<FriendRequest>() {
                     @Override
                     public void onResponse(Call<FriendRequest> call, Response<FriendRequest> response) {
-                        if(response.code() == 201) {
-                            Toast.makeText(getActivity(), "Friend request sent", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(getActivity(), "Error. Please try again", Toast.LENGTH_SHORT).show();
+                        switch (response.code()) {
+                            case 201:
+                                Toast.makeText(getActivity(), "Friend request sent", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                Toast.makeText(getActivity(), "Error. Please try again", Toast.LENGTH_SHORT).show();
+                                break;
                         }
                     }
-
                     @Override
-                    public void onFailure(Call<FriendRequest> call, Throwable t) {
-                    }
+                    public void onFailure(Call<FriendRequest> call, Throwable t) {}
                 });
             }
         });
