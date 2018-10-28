9afbf5fe8e858bf8d3e01109e6b71f96222f0b1f
Justus Chua <justuschua@hotmail.com>
2018-10-28 19:27:22 +0800

    remove unused routes for organizations and students

--- node-rest/api/routes/organizations.js
+++ node-rest/api/routes/organizations.js
@@ -11,10 +11,4 @@ router.get('/', checkAuth, OrgController.org_get_all);
 // Handles POST requests (Create an organization name card)
 router.post('/create', checkAuth, OrgController.org_create_card);
 
-// Handles GET requests for an organization card(Retrieve info from one card)
-router.get('/:cardId', checkAuth, OrgController.org_get_one);
-
-// Handles DELETE requests (Deletes one organization card)
-router.delete('/:cardId', OrgController.org_delete_one);
-
 module.exports = router;
